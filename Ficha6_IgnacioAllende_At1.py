7c58576635ca84e574b98240c44078ad79f2d9fa
Ignacio01000001 <allende.ignacio100@gmail.com>
2026-08-04 18:04:47 +0100

    Add files via upload

---
+++ Ficha6_IgnacioAllende_At1.py
@@ -0,0 +1,33 @@
+import turtle as t
+print("Digite a figura que quer (quadrado/circulo/retangulo)")
+forma=str(input())
+print("Digite a cor que quer em inglês(ex: red, blue, black)")
+cor=str(input())
+if forma=="quadrado":
+    print("Digite o tamanho do lado em um numero inteiro")
+    front = int(input())
+    def quadrado(cor, veloc, front, direita):
+        for angulo in range(0, 100, 15):
+            t.Turtle()
+            t.color(cor)
+            t.speed(veloc)
+            t.forward(front)
+            t.right(direita)
+    t.reset()
+    t.shape("turtle")
+    quadrado(cor,2,front,90)
+    t.done()
+elif forma=="circulo":
+    def circulo(cor,x,y,veloc,raio):
+        for angulo in range (0,1,1):
+            t.goto(x,y)
+            t.color(cor)
+            t.speed(veloc)
+            t.seth(angulo)
+            t.circle(raio)
+    t.reset()
+    t.shape("turtle")
+    circulo(cor,0,0,100,100)
+    t.done()
+else:
+    print("Algum dado inserido não está correto")
